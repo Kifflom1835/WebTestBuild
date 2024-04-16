d7cece29dda974bc4e5ce6b6a426c7a77ddbb68b
Viktor Shylumov <shylum033@gmail.com>
2024-04-16 20:06:03 +0300

    add source

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {UnityContainer} from "./components/UnityContainer/UnityContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <UnityContainer />
+    </div>
+  );
+}
+
+export default App;
